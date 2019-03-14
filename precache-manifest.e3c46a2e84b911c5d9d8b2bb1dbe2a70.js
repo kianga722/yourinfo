db74cfc9f9b7c1c927fdac11855144484a6ec126
kianga722 <kianga722@gmail.com>
2019-03-13 22:12:05 -0400

    Updates

---
+++ precache-manifest.e3c46a2e84b911c5d9d8b2bb1dbe2a70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab5a822169bb0d08735a",
+    "url": "/yourinfo/static/js/runtime~main.ab5a8221.js"
+  },
+  {
+    "revision": "06bc1f910bf78c8db9ae",
+    "url": "/yourinfo/static/js/main.06bc1f91.chunk.js"
+  },
+  {
+    "revision": "fedd96ad366c1c1529ae",
+    "url": "/yourinfo/static/js/1.fedd96ad.chunk.js"
+  },
+  {
+    "revision": "06bc1f910bf78c8db9ae",
+    "url": "/yourinfo/static/css/main.8a5ead50.chunk.css"
+  },
+  {
+    "revision": "072b610f13ab2d8aa4987a5e0ec7a68d",
+    "url": "/yourinfo/index.html"
+  }
+];
